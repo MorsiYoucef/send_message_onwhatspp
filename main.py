3562d8c1dab99d8ce5d73306c898139b3a2a299f
MorsiYoucef <141324213+MorsiYoucef@users.noreply.github.com>
2023-08-05 17:57:09 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import pywhatkit as kit
+kit.text_to_handwriting("hello world")
+kit.info("obama")
+kit.search("python")
+kit.sendwhatmsg("+213552740578","test test ",18,00)
+kit.playonyt("workout")
